3c971acdd94d428491fbf552125a95ae97a7cddf
unknown <katepob160@gmail.com>
2023-10-12 22:45:21 +0300

    upploading files

---
+++ лаб4.2/лаб4.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double x, xp, xk, dx, A, B, y;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << fixed;
+    cout << "---------------------------" << endl;
+    cout << "|" << setw(5) << "x" << "   |"
+        << setw(7) << "y" << "   |" << endl;
+    cout << "---------------------------" << endl;
+    x = xp;
+    while (x <= xk)
+    {
+        A = tan(x) + 1;
+        if (x <= -5)
+            B = exp(x / (1 - x));
+        else if (x > -5 && x <= 31)
+            B = 3.2 + log10(1.8 * pow(x, 2));
+        else
+            B = fabs(x) * sqrt(x);
+        y = A + B;
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(10) << setprecision(3) << y
+            << " |" << endl;
+        x += dx;
+    }
+    cout << "---------------------------" << endl;
+    return 0;
+}
